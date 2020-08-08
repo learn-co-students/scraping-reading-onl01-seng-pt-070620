f4d4707f81f1b8019a6ee1870a8ab31d3fef6efd
Ebo Lee <yiboli.coding@gmail.com>
2020-08-08 23:34:13 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,3 +6,15 @@ html = open("https://flatironschool.com/")
 Nokogiri::HTML(html)
 
 doc = Nokogiri::HTML(html)
+
+puts doc
+doc.css(".headline-26OIBN").text
+
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
